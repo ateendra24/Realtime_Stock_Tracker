89d850b88d7fbfea17ae0467a97d6f5d22d3a91c
AP Solanki <88495030+ateendra24@users.noreply.github.com>
2025-11-05 19:19:05 +0530

    Implement Stock Details Page

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -28,7 +28,7 @@ const UserDropdown = ({ user, initialStocks }: { user: User, initialStocks: Stoc
             <DropdownMenuTrigger asChild>
                 <Button variant='ghost' className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
                     <Avatar className="h-8 w-8">
-                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarImage src="https://avatars.githubusercontent.com/u/88495030" />
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
                     </Avatar>
                     <div className="hidden md:flex flex-col items-start">
@@ -42,7 +42,7 @@ const UserDropdown = ({ user, initialStocks }: { user: User, initialStocks: Stoc
                 <DropdownMenuLabel>
                     <div className="flex relative items-center gap-3 py-2">
                         <Avatar className="h-10 w-10">
-                            <AvatarImage src="https://github.com/shadcn.png" />
+                            <AvatarImage src="https://avatars.githubusercontent.com/u/88495030" />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">{user.name[0]}</AvatarFallback>
                         </Avatar>
                         <div className="flex flex-col">
